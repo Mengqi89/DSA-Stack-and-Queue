486572166a97db467f7ec0a26fb25d617a9a8e54
Mengqi Wang <wmq516@gmail.com>
2019-07-18 18:28:31 -0600

    finished up to 3

--- stack.js
+++ stack.js
@@ -37,3 +37,66 @@ class Stack {
         return node.data;
     }
 }
+
+const emptyStack = new Stack()
+const starTrek = new Stack()
+starTrek.push('Kirk')
+starTrek.push('Spock')
+starTrek.push('McCoy')
+starTrek.push('Scotty')
+// console.log(starTrek.top.next)
+
+// display(): to display the stack - what is the 1st item in your stack ?
+//     Remove McCoy from your stack and display the stack
+
+function peek(stack) {
+    return stack.top.data
+}
+// console.log(peek(starTrek))
+
+function isEmpty(stack) {
+    return (stack.top === null) ? true : false
+}
+// console.log(isEmpty(starTrek))
+// console.log(isEmpty(emptyStack))
+
+function display(stack) {
+    const arr = []
+    while (stack.top !== null) {
+        arr.unshift(stack.top.data)
+        stack.top = stack.top.next
+    }
+    return arr
+}
+// console.log(display(starTrek))
+
+function is_palindrome(s) {
+    s = s.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
+    const forward = new Stack()
+    const backward = new Stack()
+
+    const arr = s.split('')
+    const arrReverse = s.split('').reverse()
+
+    for (let i = 0; i < arr.length; i++) {
+        forward.push(arr[i])
+    }
+    for (let i = 0; i < arrReverse.length; i++) {
+        backward.push(arrReverse[i])
+    }
+    //forward.top === null empty stack
+    while (forward.top !== null) {
+        if (forward.top.data !== backward.top.data) {
+            return false
+        }
+        forward.top = forward.top.next
+        backward.top = backward.top.next
+    }
+    return true
+}
+
+// True, true, true, false
+console.log(is_palindrome("dad"));
+console.log(is_palindrome("A man, a plan, a canal: Panama"));
+console.log(is_palindrome("1001"));
+console.log(is_palindrome("Tauhida"));
